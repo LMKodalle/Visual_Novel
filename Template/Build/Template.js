b9106765ba447daa560687cec8be6dc056904039
LMKodalle <44026608+LMKodalle@users.noreply.github.com>
2021-12-07 19:30:37 +0100

    test splash

--- Template/Build/Template.js
+++ Template/Build/Template.js
@@ -54,19 +54,20 @@ var Template;
 (function (Template) {
     async function Scene() {
         console.log("Intro");
-        let text = {
+        /*  let text = {
             narrator: {
-                T001: "",
-                T002: ""
+              T001: "",
+              T002: ""
             },
-            protagonist: {}
-        };
+            protagonist: {
+            }
+          } */
         await Template.ƒS.Location.show(Template.location.bedroom);
         await Template.ƒS.update(Template.transition.puzzle.duration, Template.transition.puzzle.alpha, Template.transition.puzzle.edge);
-        await Template.ƒS.Character.show(Template.characters.protagonist, Template.characters.protagonist.pose.happy, Template.ƒS.positions.bottomcenter);
-        await Template.ƒS.update(1);
-        await Template.ƒS.Speech.tell(Template.characters.narrator, text.narrator.T001);
-        await Template.ƒS.Character.hide(Template.characters.protagonist);
+        //await ƒS.Character.show(characters.protagonist, characters.protagonist.pose.happy, ƒS.positions.bottomcenter);
+        //await ƒS.update(1);
+        //await ƒS.Speech.tell(characters.narrator, text.narrator.T001);
+        //await ƒS.Character.hide(characters.protagonist);
     }
     Template.Scene = Scene;
 })(Template || (Template = {}));
--- Template/Source/Scene.ts
+++ Template/Source/Scene.ts
@@ -2,20 +2,20 @@ namespace Template {
   export async function Scene(): ƒS.SceneReturn {
     console.log("Intro");
     
-    let text = {
+  /*  let text = {
       narrator: {
         T001: "",
         T002: ""
       },
       protagonist: {
       } 
-    }
+    } */
     await ƒS.Location.show(location.bedroom);
     await ƒS.update(transition.puzzle.duration, transition.puzzle.alpha, transition.puzzle.edge);
-    await ƒS.Character.show(characters.protagonist, characters.protagonist.pose.happy, ƒS.positions.bottomcenter);
-    await ƒS.update(1);
-    await ƒS.Speech.tell(characters.narrator, text.narrator.T001);
-    await ƒS.Character.hide(characters.protagonist);
+    //await ƒS.Character.show(characters.protagonist, characters.protagonist.pose.happy, ƒS.positions.bottomcenter);
+    //await ƒS.update(1);
+    //await ƒS.Speech.tell(characters.narrator, text.narrator.T001);
+    //await ƒS.Character.hide(characters.protagonist);
 
 
 
